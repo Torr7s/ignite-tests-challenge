5b42a0804b7b38c54be891885bb5f8ce3da456a9
Torr7s <torresmiranda.rj@gmail.com>
2022-04-25 14:19:13 -0300

    [tests]: Create CreateStatementUseCase test

---
+++ src/modules/statements/useCases/createStatement/__tests__/CreateStatementUseCase.spec.ts
@@ -0,0 +1,70 @@
+import { User } from '@modules/users/entities/User';
+
+import { InMemoryStatementsRepository } from '../../../repositories/in-memory/InMemoryStatementsRepository';
+import { InMemoryUsersRepository } from '@modules/users/repositories/in-memory/InMemoryUsersRepository';
+
+import { CreateStatementUseCase } from '../CreateStatementUseCase';
+
+import { AuthenticateUserUseCase } from '@modules/users/useCases/authenticateUser/AuthenticateUserUseCase';
+import { CreateUserUseCase } from '@modules/users/useCases/createUser/CreateUserUseCase';
+import { ICreateUserDTO } from '@modules/users/useCases/createUser/ICreateUserDTO';
+
+let statementsRepositoryInMemo: InMemoryStatementsRepository;
+let usersRepositoryInMemo: InMemoryUsersRepository;
+
+let createStatementUseCase: CreateStatementUseCase;
+let authUserUseCase: AuthenticateUserUseCase;
+let createUserUseCase: CreateUserUseCase;
+
+enum OperationType {
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
+}
+
+describe('CreateStatementUseCase', () => {
+  beforeEach(() => {
+    statementsRepositoryInMemo = new InMemoryStatementsRepository()
+    usersRepositoryInMemo = new InMemoryUsersRepository()
+
+    createStatementUseCase = new CreateStatementUseCase(
+      usersRepositoryInMemo,
+      statementsRepositoryInMemo
+    )
+    authUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemo)
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemo)
+  })
+
+  it('should be defined', () => {
+    expect(statementsRepositoryInMemo).toBeDefined()
+    expect(usersRepositoryInMemo).toBeDefined()
+    expect(createStatementUseCase).toBeDefined()
+    expect(authUserUseCase).toBeDefined()
+    expect(createUserUseCase).toBeDefined()
+  })
+
+  it('should be able to create a new deposit statement', async () => {
+    const user = await createUserUseCase.execute({
+      name: 'test_user',
+      email: 'test_user@gmail.com',
+      password: 'test_password'
+    })
+
+    await authUserUseCase.execute({
+      email: 'test_user@gmail.com',
+      password: 'test_password'
+    })
+
+    const userDepositStatement = await createStatementUseCase.execute({
+      user_id: user.id,
+      type: OperationType.DEPOSIT,
+      description: 'Test deposit statement',
+      amount: 10
+    })
+
+    expect(userDepositStatement).toHaveProperty('id')
+    expect(userDepositStatement).toHaveProperty('user_id')
+    expect(userDepositStatement).toHaveProperty('type')
+    expect(userDepositStatement).toHaveProperty('description')
+    expect(userDepositStatement).toHaveProperty('amount')
+  })
+})
