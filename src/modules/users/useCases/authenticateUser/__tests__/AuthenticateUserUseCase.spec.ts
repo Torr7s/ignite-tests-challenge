8f4819e8ef0058983e5fa1c029a26571b37d5876
Torr7s <torresmiranda.rj@gmail.com>
2022-04-25 11:01:05 -0300

    [tests]: Create AuthUserUseCase test

---
+++ src/modules/users/useCases/authenticateUser/__tests__/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,42 @@
+import { ICreateUserDTO } from '../../createUser/ICreateUserDTO';
+import { IAuthenticateUserResponseDTO } from '../IAuthenticateUserResponseDTO';
+
+import { InMemoryUsersRepository } from '@modules/users/repositories/in-memory/InMemoryUsersRepository';
+
+import { AuthenticateUserUseCase } from '../AuthenticateUserUseCase';
+import { CreateUserUseCase } from '@modules/users/useCases/createUser/CreateUserUseCase';
+
+let authUserUseCase: AuthenticateUserUseCase
+let createUserUseCase: CreateUserUseCase;
+let usersRepositoryInMemo: InMemoryUsersRepository;
+
+describe('AuthenticateUserUseCase', (): void => {
+  beforeEach(() => {
+    usersRepositoryInMemo = new InMemoryUsersRepository()
+    authUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemo)
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemo)
+  })
+
+  it('should be defined', async (): Promise<void> => {
+    expect(usersRepositoryInMemo).toBeDefined()
+    expect(authUserUseCase).toBeDefined()
+    expect(createUserUseCase).toBeDefined()
+  })
+
+  it('should be able to authenticate an user', async (): Promise<void> => {
+    const user: ICreateUserDTO = {
+      name: 'test_user',
+      email: 'test_user@gmail.com',
+      password: 'test_password'
+    }
+
+    await createUserUseCase.execute(user)
+
+    const userAuth: IAuthenticateUserResponseDTO = await authUserUseCase.execute({
+      email: user.email,
+      password: user.password
+    })
+
+    expect(userAuth).toHaveProperty('token')
+  })
+})
